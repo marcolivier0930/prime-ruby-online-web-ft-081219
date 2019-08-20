3d94960d33126e5b7c4010b7b111b3abfeb33346
Marc Blanchard <marcblanchard0930@gmail.com>
2019-08-20 00:35:47 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,10 @@
 # Add  code here!
-def prime?(num)
-  (2..integer - 1).each {|x| return false if (integer % x) == 0 }
-  true
+def prime?(integer)
+    return false if integer < 2
+    (2..integer - 1).each do |x|
+        if (integer % x) == 0
+            return false
+        end
+    end
+    true
 end
